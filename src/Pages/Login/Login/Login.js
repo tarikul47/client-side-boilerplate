0230f934093ef631fdbd714ff7cbde1a6d3ae0a2
Tarikul Islam <tarikul47@gmail.com>
2021-11-08 13:50:43 +0600

    Login page design done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import { Button, Container, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import loginImg from './../../../images/login.png';
+
 
 const Login = () => {
+
+    const [LoginData, setLoginData] = useState({});
+
+    const handleOnChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newLoginData = { ...LoginData }
+        //console.log(field, value);
+        newLoginData[field] = value;
+        setLoginData(newLoginData);
+        console.log(LoginData);
+    }
+    const handleLoginSubmit = e => {
+        e.preventDefault();
+        //alert('hello');
+    }
     return (
-        <div>
-            <h2>Login</h2>
-        </div>
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="body1" gutterBottom>
+                        Login
+                    </Typography>;
+                    <form onSubmit={handleLoginSubmit}>
+                        <TextField
+                            id="standard-basic"
+                            label="Your Email"
+                            variant="standard"
+                            name="email"
+                            onChange={handleOnChange}
+                            sx={{ width: '75%', m: 1 }}
+                        />
+                        <TextField
+                            id="standard-basics"
+                            label="Your Password"
+                            variant="standard"
+                            type="password"
+                            name="password"
+                            onChange={handleOnChange}
+                            sx={{ width: '75%', m: 1 }}
+                        />
+                        <Button sx={{ width: '75%', m: 3 }} type="submit" variant="contained">Login</Button>
+                    </form>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{ width: '100%' }} src={loginImg} alt="" />
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
 export default Login;
+
