d3aef47b913b33da7521e681447a2afa2f611704
Tarikul Islam <tarikul47@gmail.com>
2021-11-08 21:59:27 +0600

    Some issue fixed

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ function App() {
         <Router>
           <Header></Header>
           <Switch>
+            <Route path="/">
+              <Home></Home>
+            </Route>
             <Route path="/home">
               <Home></Home>
             </Route>
